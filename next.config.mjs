bb0426d386747e9b802e3d68a10c645b29f796ce
alifaiizan <alifaizanahmad@gmail.com>
2024-02-26 02:50:36 +0100

    init second

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    crossOrigin: 'anonymous',	
+    images: {
+        domains: ['res.cloudinary.com','uploadthing.com','utfs.io','img.clerk.com','subdomain','files.stripe.com'],
+    },
+    reactStrictMode: false,
+};
 
 export default nextConfig;
---
+++ src/app/(main)/agency/(auth)/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SignUp } from "@clerk/clerk-react";
+
+const page = () => {
+  return (
+    <SignUp />
+  )
+}
+
+export default page
